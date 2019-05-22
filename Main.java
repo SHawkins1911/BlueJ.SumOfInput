2230ec5c0eac78e699b22b3722d2a968d2cd4086
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro-8.local>
2019-05-22 18:52:03 -0400

    done

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
+        System.out.println("Please give me a number");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum = 0;
+        for(int i=0; i < n; i++){
+            sum += i;
+        
+        };
+   
+        
+        System.out.print(sum);
     }
 }
